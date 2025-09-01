851e14b75669c17c009d931375cd75d72bba5115
Tom Howard <t.howard@sheffield.ac.uk>
2025-09-01 15:46:09 +0000

    Tidy up turn.py

--- scripts/turn.py
+++ scripts/turn.py
@@ -11,10 +11,10 @@ from nav_msgs.msg import Odometry
 from com_offer_holder_days_modules.tb3_tools import quaternion_to_euler
 from math import degrees
 
-class Square(Node):
+class Turn(Node):
 
     def __init__(self):
-        super().__init__("move_square")
+        super().__init__("node")
 
         self.vel_pub = self.create_publisher(
             msg_type=Twist,
@@ -31,23 +31,20 @@ class Square(Node):
 
         self.vel_msg = Twist()
         self.first_message = False
-        self.turn = False 
-        
         self.done_future = Future()
 
         ctrl_rate = 10 # hz
         self.timer = self.create_timer(
             timer_period_sec=1/ctrl_rate,
-            callback=self.timer_callback,
+            callback=self.exec_turn,
         )
 
         self.declare_parameter('yaw_ang', 45)
         self.yaw_ang_request = self.get_parameter('yaw_ang').get_parameter_value().integer_value
 
-        self.x = 0.0; self.y = 0.0; self.theta_z = 0.0
-        self.xref = 0.0; self.yref = 0.0; self.theta_zref = 0.0
+        self.theta_z = 0.0
+        self.theta_zref = 0.0
         self.yaw = 0.0 # a variable to keep track of how far the robot has turned
-        self.displacement = 0.0 # a variable to keep track of how far the robot has moved
              
         self.shutdown = False
         
@@ -62,21 +59,16 @@ class Square(Node):
 
     def odom_callback(self, msg_data: Odometry):
         pose = msg_data.pose.pose 
-        
-        self.x = pose.position.x 
-        self.y = pose.position.y
 
-        (roll, pitch, yaw) = quaternion_to_euler(pose.orientation)
+        (_, _, yaw) = quaternion_to_euler(pose.orientation)
 
         self.theta_z = degrees(abs(yaw)) # abs(yaw) makes life much easier!!
 
         if not self.first_message: 
             self.first_message = True
-            self.xref = self.x
-            self.yref = self.y
             self.theta_zref = self.theta_z
 
-    def timer_callback(self):
+    def exec_turn(self):
         # turn by X degrees...
         # keep track of how much yaw has been accrued during the current turn
         self.yaw = self.yaw + abs(self.theta_z - self.theta_zref)
@@ -84,10 +76,7 @@ class Square(Node):
         if self.yaw >= self.yaw_ang_request:
             # That's enough, stop turning!
             self.vel_msg = Twist()
-            self.turn = False
             self.yaw = 0.0
-            self.xref = self.x
-            self.yref = self.y
             self.get_logger().info(
                 "Stopped."
             )
@@ -108,18 +97,18 @@ def main(args=None):
         args=args,
         signal_handler_options=SignalHandlerOptions.NO,
     )
-    move_square = Square()
+    node = Turn()
     try:
-        rclpy.spin_until_future_complete(move_square, move_square.done_future)
+        rclpy.spin_until_future_complete(node, node.done_future)
     except KeyboardInterrupt:
         print(
-            f"{move_square.get_name()} received a shutdown request (Ctrl+C)."
+            f"{node.get_name()} received a shutdown request (Ctrl+C)."
         )
     finally:
-        move_square.on_shutdown()
-        while not move_square.shutdown:
+        node.on_shutdown()
+        while not node.shutdown:
             continue
-        move_square.destroy_node()
+        node.destroy_node()
         rclpy.shutdown()
 
 if __name__ == "__main__":
